073d5fb5270815043445444d0db3a4e2f4b1f440
ehabelhelw <ehabelhelw@yahoo.com>
2024-11-16 19:00:46 +0200

    whky mohamed ehab

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -46,6 +46,13 @@ public class PersonService {
 		p.setFirstName("webhook");
 		p.setLastName("testing");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("6");
+		p.setAge(32);
+		p.setFirstName("mohamed");
+		p.setLastName("ehab");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
